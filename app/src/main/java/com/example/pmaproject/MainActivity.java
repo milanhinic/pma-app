d04686f88434398b637f7fe4ad8c882b4e687ef9
Milan <milanhinic2@gmail.com>
2019-04-17 21:40:54 +0200

    v1: ListViewAdapter

--- app/src/main/java/com/example/pmaproject/MainActivity.java
+++ app/src/main/java/com/example/pmaproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pmaproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        Intent intent = new Intent(getApplicationContext(), ListViewActivity.class);
+//        startActivity(intent);
+
     }
 
 
